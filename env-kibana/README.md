39c4e29ec5ed3aa4ba48afa4ce9ed515b503721e
hcoa <ssokol@softserveinc.com>
2016-03-28 17:19:39 +0300

    add env-kibana

---
+++ env-kibana/README.md
@@ -0,0 +1,7 @@
+If you want to have external access, you can add externalIps to kibana-service.yml like:
+
+```
+  externalIPs:
+      - "<ip>"
+```
+
---
+++ env-kibana/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Fail hard and fast
+set -eo pipefail
+
+# Elastic ENV variables
+export ELASTIC_NAME=${ELASTIC_NAME:-elastic}
+export ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-localhost}
+export ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-9200}
+
+# Create workiing folders and fix permissions
+mkdir -p /data/$ELASTIC_NAME; chown -fR elastic:elastic /data/$ELASTIC_NAME
+
+# add elastic host and port to config
+sed -i -e "s/__ELASTICSEARCH_HOST__/$ELASTICSEARCH_HOST/g" -e "s/__ELASTICSEARCH_PORT__/$ELASTICSEARCH_PORT/g" "$KIBANA_CONFIG"
+
+# Drop root privileges if we are running elasticsearch
+if [ "${3:0:4}" = 'node' ]; then
+	exec gosu elastic "$@"
+fi
+
+# As argument is not related to elasticsearch, then assume that user wants to run his own process, for example a `bash` shell to explore this image
+exec "$@"
