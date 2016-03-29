072f9160918f9bf93c6eea80c9905829faa6344c
sporokh@softserveinc.com <sporokh@softserveinc.com>
2016-03-29 16:30:01 +0300

    Added kibana global envs

--- env-kibana/docker-entrypoint.sh
+++ env-kibana/docker-entrypoint.sh
@@ -5,14 +5,15 @@ set -eo pipefail
 
 # Elastic ENV variables
 export ELASTIC_NAME=${ELASTIC_NAME:-elastic}
-export ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-localhost}
-export ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-9200}
+# Commented cause using Docker Global values
+#export ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-localhost}
+#export ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-9200}
 
 # Create workiing folders and fix permissions
 mkdir -p /data/$ELASTIC_NAME; chown -fR elastic:elastic /data/$ELASTIC_NAME
 
 # add elastic host and port to config
-sed -i -e "s/__ELASTICSEARCH_HOST__/$ELASTICSEARCH_HOST/g" -e "s/__ELASTICSEARCH_PORT__/$ELASTICSEARCH_PORT/g" "$KIBANA_CONFIG"
+sed -i -e "s/__ELASTICSEARCH_HOST__/$ELASTIC_CLIENT_PORT_9200_TCP_ADDR/g" -e "s/__ELASTICSEARCH_PORT__/$ELASTIC_CLIENT_PORT_9200_TCP_PORT/g" "$KIBANA_CONFIG"
 
 # Drop root privileges if we are running elasticsearch
 if [ "${3:0:4}" = 'node' ]; then
